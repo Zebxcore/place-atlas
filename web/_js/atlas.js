1a2288cde88213830cc52dafc230db3e51aab8ac
Sebastian Cortes <82243903+Zebxcore@users.noreply.github.com>
2022-04-04 23:10:39 -0400

    PR: Add Kaguya-sama
    Kaguya-sama art from kaguya-sama server.

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,8 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
-];
+{"id":179, "name": "Kaguya-sama: Love is War","description": "Guyasmug, Shirogane, Kaguya, Hayasaka, and samll portraits of more character of the manga series Kaguya-sama: Love is War made by its discord server","website": "https://discord.gg/kaguya","subreddit": "https://www.reddit.com/r/Kaguya_sama/","center": [0.5,0.5],"path": []}];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
 
